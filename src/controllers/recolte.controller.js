bf83566eab6ebe129caf5afe9f207c697aa602a5
muniecodechristian <muniecodechristian@gmail.com>
2025-12-03 14:57:22 +0100

    Add files via upload

---
+++ src/controllers/recolte.controller.js
@@ -0,0 +1,95 @@
+import asyncHandler from "express-async-handler";
+import Recolte from "../models/recoltes.models.js";
+import User from "../models/user.model.js";
+import { getAuth } from "@clerk/express";
+import cloudinary from "../config/cloudinary.js";
+
+
+
+
+
+
+export  const createRecoltePost = asyncHandler(async (req, res) => {
+        const { userId } = getAuth(req);
+        const user = await User.findOne({ clerkId: userId });
+        if (!user) return res.status(404).json({ error: "User not found" });
+    
+        const { images, title,phone ,  description,price,category,quantity, city,country } = req.body;
+    
+         const recolte = await Recolte.create({
+            user: user._id,
+              images,
+               title,
+               phone ,
+                 description,
+                 price,
+                 category,
+                 quantity,
+                  city,
+                  country
+         
+        });
+    
+        res.status(201).json({ recolte });
+    });
+
+
+
+
+
+
+export const getRecoltePosts = asyncHandler(async (req, res) => {
+        const recoltes = await Recolte.find()
+            .sort({ createdAt: -1 })
+            .populate("user", "username firstName lastName profilePicture")
+         
+    
+        res.status(200).json({ recoltes });
+    });
+
+
+
+
+
+
+export const getMyRecoltePost = asyncHandler(async (req, res) => {
+    const { userId } = getAuth(req);
+    const user = await User.findOne({ clerkId: userId });
+    if (!user) return res.status(404).json({ error: "User not found" });
+    const recoltes = await Recolte.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate("user", "username firstName lastName profilePicture")
+     
+  
+    res.status(200).json({ recoltes });
+  });
+
+
+  export const deleteRecoltePost = asyncHandler(async (req, res) => {
+    const { userId } = getAuth(req);
+    const { recolteId } = req.params;
+  
+    const user = await User.findOne({ clerkId: userId });
+    if (!user) return res.status(404).json({ error: "User not found" });
+  
+    const recolte = await Recolte.findById(recolteId);
+  
+    if (!recolte) return res.status(404).json({ error: "Recolte not found" });
+  
+    if (recolte.user.toString() !== user._id.toString()) {
+      return res.status(403).json({ error: "You can only delete your own recolte posts" });
+    }
+  
+    // delete the recolte
+    await Recolte.findByIdAndDelete(recolteId);
+  
+    res.status(200).json({ message: "Recolte deleted successfully" });
+  });
+
+
+
+  
+
+
+
+  
