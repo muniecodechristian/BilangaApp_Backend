322cec4c9615f3bdc6a8b39276ea96f5e5521d17
muniecodechristian <muniecodechristian@gmail.com>
2026-02-15 17:02:12 +0100

    Update server response message

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(clerkMiddleware());
 app.use(arcjetMiddleware);
 
-app.get("/", (req, res) => res.send("Hello from server"));
+app.get("/", (req, res) => res.send("Hello from server update" ));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
