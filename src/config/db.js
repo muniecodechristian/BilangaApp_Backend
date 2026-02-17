cc580d8291ee12c43b9d6472f9eb02179ef6ba6d
muniecodechristian <muniecodechristian@gmail.com>
2026-02-17 08:05:23 +0100

    Change MongoDB connection string in db.js
    Updated MongoDB connection string to use a specific user and cluster.

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(ENV.MONGO_URI);
+    await mongoose.connect('mongodb+srv://fatshiemploi_db_user:1a1oJr1PzTO43GqB@cluster0.lsfavcb.mongodb.net/');
     console.log("Connected to DB SUCCESSFULLY ✅");
   } catch (error) {
     console.log("Error connecting to MONGODB");
