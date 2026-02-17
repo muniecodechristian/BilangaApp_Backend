841f9b79f4b903d2d31329796ed6e985cb042a5f
muniecodechristian <muniecodechristian@gmail.com>
2026-02-17 08:10:20 +0100

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ import { ENV } from "./env.js";
 export const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://fatshiemploi_db_user:1a1oJr1PzTO43GqB@cluster0.lsfavcb.mongodb.net/');
-    console.log("Connected to DB SUCCESSFULLY ✅");
+    console.log("Connected to DB SUCCESSFULLY ✅ ");
   } catch (error) {
     console.log("Error connecting to MONGODB");
     process.exit(1);
