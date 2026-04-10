33616bd6894bcc3b8d249f5dfc64679a492d1966
muniecodechristian <muniecodechristian@gmail.com>
2026-04-10 16:59:08 +0100

    Update MongoDB connection string to use environment variable

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://fatshiemploi_db_user:1a1oJr1PzTO43GqB@cluster0.lsfavcb.mongodb.net/');
-    console.log("Connected to DB SUCCESSFULLY ✅  ");
+    await mongoose.connect(process.env.mongo_uri);
+    console.log("Connected to DB SUCCESSFULLY  ");
   } catch (error) {
     console.log("Error connecting to MONGODB");
     process.exit(1);
